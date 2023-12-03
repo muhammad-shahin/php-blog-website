d24de584e2387cb67841ef125c91ae0e564e0612
muhammad-shahin <muhammadshahin002@gmail.com>
2023-12-03 21:41:30 +0600

    template complete

---
+++ admin/template.php
@@ -0,0 +1,22 @@
+<?php
+include_once("includes/template_head.php")
+?>
+
+
+<body class="sb-nav-fixed">
+    <?php include_once("includes/topnav.php") ?>
+    <div id="layoutSidenav">
+        <?php include_once("includes/sidenav.php") ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+
+                </div>
+            </main>
+            <?php include_once("includes/footer.php") ?>
+        </div>
+    </div>
+    <?php include_once("includes/template_script.php") ?>
+</body>
+
+</html>
